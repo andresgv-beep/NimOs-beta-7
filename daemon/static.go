b26c5297da332b6bc3e8c5e24db0e99fa6a3fc77
andresgv-beep <andresgv7455@gmail.com>
2026-04-09 21:23:43 +0200

    Add files via upload

--- daemon/static.go
+++ daemon/static.go
@@ -94,10 +94,10 @@ func serveStatic(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Security-Policy",
 					"default-src 'self'; "+
 						"script-src 'self' 'unsafe-inline'; "+
-						"style-src 'self' 'unsafe-inline'; "+
+						"style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "+
 						"img-src 'self' data: blob:; "+
 						"connect-src 'self'; "+
-						"font-src 'self'; "+
+						"font-src 'self' https://fonts.gstatic.com; "+
 						"frame-src 'self'; "+
 						"frame-ancestors 'self'; "+
 						"object-src 'none'; "+
