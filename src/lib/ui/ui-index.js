b0a5912e740b0a20456fa9ffdfc8c8ab2a5a892b
andresgv-beep <andresgv7455@gmail.com>
2026-04-11 20:46:13 +0200

    Add files via upload

---
+++ src/lib/ui/ui-index.js
@@ -0,0 +1,7 @@
+export { default as Card } from './Card.svelte';
+export { default as Button } from './Button.svelte';
+export { default as Badge } from './Badge.svelte';
+export { default as SectionLabel } from './SectionLabel.svelte';
+export { default as InfoGrid } from './InfoGrid.svelte';
+export { default as DiskTable } from './DiskTable.svelte';
+export { default as StatusHero } from './StatusHero.svelte';
